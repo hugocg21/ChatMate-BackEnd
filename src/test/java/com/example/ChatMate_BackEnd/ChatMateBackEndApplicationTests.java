b569d252c7b512db0259fcc97911c3b6af93b62e
hugocg21 <hugocastanogonzalez@gmail.com>
2024-07-22 02:47:22 +0200

    first commit

---
+++ src/test/java/com/example/ChatMate_BackEnd/ChatMateBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ChatMate_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatMateBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
